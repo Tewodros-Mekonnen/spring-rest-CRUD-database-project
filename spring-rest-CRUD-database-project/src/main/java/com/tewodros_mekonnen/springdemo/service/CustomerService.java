f74228210c021a569a267f2760384e20f41b9e03
tewodros-mekonnen <you@example.com>
2020-08-26 02:28:56 -0500

    initial commit

---
+++ spring-rest-CRUD-database-project/src/main/java/com/tewodros_mekonnen/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.tewodros_mekonnen.springdemo.service;
+
+import java.util.List;
+
+import com.tewodros_mekonnen.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
---
+++ spring-rest-CRUD-database-project/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+<h3>Spring CRM REST Demo</h3>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/api/customers" >Get List of Customers</a>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/api/customer/2" >Get a Single Customer</a>
+
+</body>
+</html>
